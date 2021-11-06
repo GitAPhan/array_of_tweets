aff847b2be1de5c7c89231da32bb46ac2279cedb
GitAPhan <itsandrewphan@gmail.com>
2021-11-06 16:56:01 -0600

    completed W9E

--- js/app.js
+++ js/app.js
@@ -11,6 +11,90 @@ var twitterPost = {
 var tweet_length = twitterPost.tweet.length;
 
 // this is the loop to print out tweet, username and created_at field
-for(var count = 0; count < tweet_length; count++) {
+for (var count = 0; count < tweet_length; count++) {
     console.log(`'${twitterPost.tweet[count]}' - ${twitterPost.username[count]} (${twitterPost.created_at[count]}) `);
+};
+
+
+// this is to rearrange my advanced array
+var tweets = [
+    {
+        content: twitterPost.tweet[0],
+        username: twitterPost.username[0],
+        created_at: twitterPost.created_at[0],
+        age: twitterPost.age[0],
+
+    }, {
+        content: twitterPost.tweet[1],
+        username: twitterPost.username[1],
+        created_at: twitterPost.created_at[1],
+        age: twitterPost.age[1],
+
+    }, {
+        content: twitterPost.tweet[2],
+        username: twitterPost.username[2],
+        created_at: twitterPost.created_at[2],
+        age: twitterPost.age[2],
+
+    }, {
+        content: twitterPost.tweet[3],
+        username: twitterPost.username[3],
+        created_at: twitterPost.created_at[3],
+        age: twitterPost.age[3],
+
+    }, {
+        content: twitterPost.tweet[4],
+        username: twitterPost.username[4],
+        created_at: twitterPost.created_at[4],
+        age: twitterPost.age[4],
+
+    }, {
+        content: twitterPost.tweet[5],
+        username: twitterPost.username[5],
+        created_at: twitterPost.created_at[5],
+        age: twitterPost.age[5],
+
+    }, {
+        content: twitterPost.tweet[6],
+        username: twitterPost.username[6],
+        created_at: twitterPost.created_at[6],
+        age: twitterPost.age[6],
+
+    }, {
+        content: twitterPost.tweet[7],
+        username: twitterPost.username[7],
+        created_at: twitterPost.created_at[7],
+        age: twitterPost.age[7],
+
+    }, {
+        content: twitterPost.tweet[8],
+        username: twitterPost.username[8],
+        created_at: twitterPost.created_at[8],
+        age: twitterPost.age[8],
+
+    }, {
+        content: twitterPost.tweet[9],
+        username: twitterPost.username[9],
+        created_at: twitterPost.created_at[9],
+        age: twitterPost.age[9],
+
+    },
+]
+
+// function to verify age is above 17
+function ageVerify(num) {
+    if (num['age'] >= 18) {
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// declare variable to represent filtered list
+var AgeVerfiedTweets = tweets.filter(ageVerify);
+
+// print out filter list in a legible format
+console.log('*****Below is the filtered list of tweets of users over the age of 17*****')
+for (var count = 0; count < AgeVerfiedTweets.length; count++) {
+    console.log(`'${AgeVerfiedTweets[count]['content']}' - ${AgeVerfiedTweets[count]['username']} (${AgeVerfiedTweets[count]['created_at']}) AGE:${AgeVerfiedTweets[count]['age']}`);
 };
